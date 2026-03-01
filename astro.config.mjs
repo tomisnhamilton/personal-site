4f6986d1cd5b53b5ac452d1ebe2a2c0db5df2cbe
Tomis Hamilton <tomisnhamilton@gmail.com>
2026-02-28 22:33:20 -0700

    UPD: add icons to contacts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
 
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), icon()],
   markdown: {
     shikiConfig: {
       // 'css-variables' allows us to style the code with our Gold accent
